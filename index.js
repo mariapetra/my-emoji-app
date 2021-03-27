e22e68c39ae1151335d141d55640117f0f5c9b10
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2021-03-27 14:41:01 +0800

    added buttons

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 
 const myEmojis = ["🧑‍💻", "🧘", "📷"]
-const emojiContainer = document.getElementById("emojiContainer")
-console.log(emojiContainer)
-
+const emojiContainer = document.getElementById("emoji-container")
 
 for (let i = 0; i < myEmojis.length; i++) {
         // console.log(myEmojis[i])
@@ -12,5 +10,18 @@ for (let i = 0; i < myEmojis.length; i++) {
     emojiContainer.append(reneredEmojis)
 }
 
+// Hook an event listener up with the button, and log out the input field's value when it's clicked
+function handleButtonClick() {
+    btn.style.color = "green"
+}
+
+const btnClick = document.getElementById("push-btn")
+
+function handleButtonClick() {
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput.value)
+}
+
+btnClick.addEventListener("click", handleButtonClick) 
 
  
